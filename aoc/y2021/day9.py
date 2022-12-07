025e20a61f218bec196ea1dc0677dd5a78b62c06
Marcus Risanger <69350948+MarcusRisanger@users.noreply.github.com>
2022-12-07 15:29:53 +0100

    Added a genius code example for posterity

--- aoc/y2021/day9.py
+++ aoc/y2021/day9.py
@@ -86,3 +86,29 @@ if __name__ == "__main__":
 
     puzzle.answer_a, troughs = part1(height_map)
     puzzle.answer_b = part2(height_map, troughs)
+
+
+## Alternative (magic) solution by /u/4HbQ:
+# from math import prod
+
+# height = {(x,y):int(h) for y,l in enumerate(open(0))
+#                        for x,h in enumerate(l.strip())}
+
+# def neighbours(x, y):
+#   return filter(lambda n: n in height,  # remove points
+#     [(x,y-1),(x,y+1),(x-1,y),(x+1,y)])  #  outside grid
+
+# def is_low(p):
+#   return all(height[p] < height[n]
+#     for n in neighbours(*p))
+
+# low_points = list(filter(is_low, height))
+# print(sum(height[p]+1 for p in low_points))
+
+# def count_basin(p):
+#   if height[p] == 9: return 0  # stop counting at ridge
+#   del height[p]                # prevent further visits
+#   return 1+sum(map(count_basin, neighbours(*p)))
+
+# basins = [count_basin(p) for p in low_points]
+# print(prod(sorted(basins)[-3:]))
