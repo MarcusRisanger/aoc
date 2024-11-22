8f01adea9c1b7741495f994fde4f454c827dafe3
Marcus Risanger <69350948+MarcusRisanger@users.noreply.github.com>
2024-11-22 22:06:42 +0100

    Added 2019, day 3 part 2. Refactored.

--- aoc/y2019/day3.py
+++ aoc/y2019/day3.py
@@ -3,35 +3,71 @@ from collections import Counter
 directions = dict(U=(0, -1), D=(0, 1), L=(-1, 0), R=(1, 0))
 
 Instruction = str
-Wire = list[Instruction]
+WireInstruction = list[Instruction]
 Point = tuple[int, int]
+Step = int
+Wire = dict[Point, Step]
 
 
-def clean_input(inp: str) -> list[Wire]:
+def clean_input(inp: str) -> list[WireInstruction]:
     return [row.split(",") for row in inp.splitlines()]
 
 
 def go(pos: Point, dir: Point) -> Point:
+    """Move once from `pos` in a direction `dir`."""
     return pos[0] + dir[0], pos[1] + dir[1]
 
 
-def draw_wires(wires: list[Wire]) -> dict[Point, int]:
-    counter: dict = Counter()  # For tracking all visited coordinates
+def parse_wires(wires: list[WireInstruction]) -> list[Wire]:
+    """
+    Parse wire instructions into a list of `Wire` objects.
+
+    A `Wire` object is a dictionary with coordinate `Point` as keys,
+    and the value is the `Step` for the wire to reach that point.
+
+    If a `Wire` crosses itself, the lowest `Step` is kept.
+    """
+    all_wires: list[dict[Point, Step]] = list()  # For tracking all visited coordinates
     for wire in wires:
-        wire_points: set[Point] = set()  # For tracking visited coordinates for this wire
+        wire_points: dict[Point, Step] = dict()  # For tracking visited coordinates for this wire
         pos = (0, 0)
+        step = 0
         for wire_element in wire:
             direction, n = wire_element[0], wire_element[1:]
             for _ in range(int(n)):
+                step += 1
                 pos = go(pos, directions[direction])
-                wire_points.add(pos)
-        counter.update(wire_points)
-    return counter
+                if pos not in wire_points:
+                    wire_points[pos] = step
+        all_wires.append(wire_points)
+    return all_wires
 
 
-def part1(wires: list[Wire]) -> str:
-    crosses_at = [coord for coord, val in draw_wires(wires).items() if val > 1]
-    return str(min(sum(map(abs, coord)) for coord in crosses_at))
+def get_crosses(wires: list[Wire]) -> list[Point]:
+    """For a list of wires, get all coordinates where any separate wires cross."""
+    counter = Counter([key for wire in wires for key in wire.keys()])
+    return [k for k, v in counter.items() if v > 1]
+
+
+def get_min_path_length(point: Point, wires: list[Wire]) -> int:
+    """For a point and list of wires, get the lowest step count for any two wires to reach point."""
+    steps = sorted(set(filter(None, (wire.get(point) for wire in wires))))
+    return steps[0] + steps[1]
+
+
+def get_wires_crosses(wires: list[WireInstruction]) -> tuple[list[Wire], list[Point]]:
+    all_wires = parse_wires(wires)
+    return all_wires, get_crosses(wires)
+
+
+def part1(wires: list[WireInstruction]) -> str:
+    _, crosses = get_wires_crosses(wires)
+    return str(min(sum(map(abs, coord)) for coord in crosses))
+
+
+def part2(wires: list[WireInstruction]) -> str:
+    all_wires, crosses = get_wires_crosses(wires)
+    return str(min(get_min_path_length(p, all_wires) for p in crosses))
 
 
 if __name__ == "__main__":
@@ -44,3 +80,4 @@ if __name__ == "__main__":
 
     # Submit answers
     puzzle.answer_a = part1(wires)
+    puzzle.answer_b = part2(wires)
