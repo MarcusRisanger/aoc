1c04bd767a6362d3bb64fdd8efaf1914f38a1791
Marcus Risanger <69350948+MarcusRisanger@users.noreply.github.com>
2024-12-19 14:24:51 +0100

    Cleanup!

--- aoc/y2023/day06.py
+++ aoc/y2023/day06.py
@@ -1,7 +1,7 @@
 from math import prod
 
 
-def clean_input(input: str) -> tuple[list[int], list[int]]:
+def clean_input(input: str) -> list[list[str]]:
     inp = input.split("\n")
     return [line.split()[1:] for line in inp]
 
@@ -10,11 +10,11 @@ def race(time, record) -> int:
     return sum(1 if t0 * (time - t0) > record else 0 for t0 in range(time))
 
 
-def part1(times: list[int], records: list[int]) -> int:
+def part1(times: list[str], records: list[str]) -> int:
     return prod(race(t, r) for t, r in zip(map(int, times), map(int, records)))
 
 
-def part2(times: list[int], records: list[int]) -> int:
+def part2(times: list[str], records: list[str]) -> int:
     return race(int("".join(times)), int("".join(records)))
 
 
@@ -27,5 +27,5 @@ if __name__ == "__main__":
     times, records = clean_input(puzzle.input_data)
 
     # Submitting answers
-    puzzle.answer_a = part1(times, records)
-    puzzle.answer_b = part2(times, records)
+    puzzle.answer_a = str(part1(times, records))
+    puzzle.answer_b = str(part2(times, records))
