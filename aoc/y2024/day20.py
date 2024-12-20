7406bb2d0d54bc1f5bc819fdc96a68deaa8184a6
Marcus Risanger <69350948+MarcusRisanger@users.noreply.github.com>
2024-12-20 10:32:37 +0100

    Nits

--- aoc/y2024/day20.py
+++ aoc/y2024/day20.py
@@ -40,8 +40,8 @@ def get_path(grid: Grid, start: Coord, end: Coord) -> Path:
 def part1(path: Path) -> int:
     count = 0
     for c in path:
-        ns = [*filter(path.get, ((c[0] + n[0], c[1] + n[1]) for n in ((0, 2), (0, -2), (2, 0), (-2, 0))))]
-        for n in ns:
+        cands = ((c[0] + n[0], c[1] + n[1]) for n in ((0, 2), (0, -2), (2, 0), (-2, 0)))
+        for n in [*filter(path.get, cands)]:
             gap = path[n] - path[c] - 2
             if gap >= 100:
                 count += 1
@@ -51,15 +51,8 @@ def part1(path: Path) -> int:
 def part2(path: Path) -> int:
     count = 0
     for c in path:
-        ns = set(
-            [
-                *filter(
-                    path.get,
-                    ((c[0] + i, c[1] + j) for i in range(-20, 21) for j in range(-20, 21) if abs(i) + abs(j) <= 20),
-                )
-            ]
-        )
-        for n in ns:
+        cands = ((c[0] + i, c[1] + j) for i in range(-20, 21) for j in range(-20, 21) if abs(i) + abs(j) <= 20)
+        for n in set([*filter(path.get, cands)]):
             gap = path[n] - path[c] - sum([abs(c[0] - n[0]), abs(c[1] - n[1])])
             if gap >= 100:
                 count += 1
